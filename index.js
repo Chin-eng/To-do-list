d4a6b4663afa5cd5bfd8c2b29c3c5c15c4ce8e37
Chinee <Chinehuu@gmail.com>
2022-08-08 21:54:21 +0800

    deletebutton

--- index.js
+++ index.js
@@ -3,21 +3,19 @@ let myList = []
 const submitBtn = document.getElementById("submit-btn");
 const clearListBtn = document.getElementById("clearList-btn");
 const inputEl = document.getElementById("input-btn");
-const ulEl =  document.getElementById("ul-el");
+const olEl =  document.getElementById("ol-el");
+const doneBtn =  document.getElementById("done-btn");
 
-
-
-    
 function render(leads) {
     let listItems = " ";
     for ( let i = 0; i < leads.length; i++) {
         listItems +=  
-            `<li>
-                ${leads[i]} <button id= "done-btn" onclick = "deleteButton()">X</button>
+            `<li id = " ">
+                ${leads[i]} <button id= "done-btn" onclick = "deleteButton(${i})">X</button>
             </li>
             `;
         }
-    ulEl.innerHTML = listItems;
+    olEl.innerHTML = listItems;
 }
 
 submitBtn.addEventListener("click", function() {
@@ -31,8 +29,9 @@ clearListBtn.addEventListener("click", function() {
     render(myList)
 })
 
-function deleteButton() {
-    
+function deleteButton(index) {
+    myList.splice(index, 1); // deletes one item starting from 'index'
+    render(myList);
 }
 
 
